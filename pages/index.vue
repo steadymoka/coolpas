7485f68769b7d94ab7f0d2bceda30c4b7ff46012
moka-a <steady.moka@gmail.com>
2025-12-29 16:35:21 +0900

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -29,34 +29,6 @@
       </div>
     </div>
 
-    <div class="fixed inset-x-0 top-0">
-      <div class="flex items-center h-20 px-8 md:px-14 md:h-24 drop-shadow-sm">
-        <Button
-          class="flex items-center space-x-3 cursor-pointer"
-          @click="explain = true"
-        >
-          <div class="text-2xl font-semibold text-white">쿨파스책방</div>
-
-          <ImageSource
-            class="transform pointer-events-none w-7 h-7 rotate-12"
-            :src="require('~/assets/images/coolcat.jpeg')"
-          />
-        </Button>
-      </div>
-    </div>
-
-    <template v-if="explain">
-      <div
-        class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-30"
-        @click="explain = false"
-      >
-        <ImageSource
-          class="object-contain w-full p-4 transition-opacity transform h-[66%] md:p-0"
-          :src="require('~/assets/images/coolpas_explain.jpeg')"
-        />
-      </div>
-    </template>
-
     <template v-if="loading">
       <DialogLoading />
     </template>
@@ -75,7 +47,6 @@ import { DEFAULT_USER_AGENT } from '~/plugins/user-agent'
 
 @Component({})
 export default class PagesIndex extends Vue {
-  explain = false
   loading = false
 
   mounted() {
